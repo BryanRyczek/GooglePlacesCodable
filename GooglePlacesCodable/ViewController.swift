0caaa121e28771e2afb39d80942eb155b416e526
Bryan Ryczek <bryanryczek@gmail.com>
2018-02-13 16:16:20 -0500

    Initial Commit

---
+++ GooglePlacesCodable/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GooglePlacesCodable
+//
+//  Created by Bryan Ryczek on 2/13/18.
+//  Copyright © 2018 Bryan Ryczek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
